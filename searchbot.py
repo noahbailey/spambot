b0a2aafd324e811d44735926370a83d1a7eabd01
Noah Bailey <43625401+noahbailey@users.noreply.github.com>
2019-02-08 19:59:20 -0500

    Fix imports and python environment

--- searchbot.py
+++ searchbot.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 
 # Retrieve the term list
 # -- Top 50,000 searches
 # -- Top 1 million websites
 
-import requests
-import random 
-import csv 
-import os, signal 
+import requests, random, csv, os, signal 
 
+# This function finds all bot browsers and kills them. 
+# Without this, some popup windows might stay open
 def check_kill_process(proc): 
     for line in os.popen("ps ax | grep " + proc + " | grep -v grep"):
         fields = line.split()
@@ -16,9 +15,9 @@ def check_kill_process(proc):
         os.kill(int(pid), signal.SIGKILL)
 
 # SERP research top 50,000 list: 
+# TODO: find better source than my old project. It seems hard to find these days... 
 print('Downloading search terms...')
 top_url = 'https://raw.githubusercontent.com/yabona/obfuscator/master/SERP_top'
-
 serp_top_raw = str((requests.get(top_url, allow_redirects=True)).content)
 serp_top = str.split(serp_top_raw, '\\n')
 
@@ -31,6 +30,7 @@ with open('top-1m.csv') as top1m_csv:
         top1m.append(row)
 
 while True:
+    # 1/5 chance of using a website instead of a search term: 
     if (random.randint(1,5) == 1): 
         searchterm = random.choice(top1m)[1]
     else: 
